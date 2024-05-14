2f21cd801e503848a0c899f97ea067c985c4d005
Nithish1321 <nithinithish2710@gmail.com>
2024-05-14 09:00:19 +0530

    deployment

--- app.js
+++ app.js
@@ -10,23 +10,22 @@ app.use(cors());
 app.use((req, res, next) => {
   next();
 });
-// app.use((req, res, next) => {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "http://localhost:3000",
-//     "http://localhost:3001"
-//   );
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000"
+  );
 
-//   res.header(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE,PATCH, OPIONS"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE,PATCH, OPIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/admin", adminRouter);
